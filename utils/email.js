c4440651257723afde2cd59b965d66af66afdc7a
Siddhesh Suryawanshi <90617470+Siddhesh002@users.noreply.github.com>
2024-03-25 23:15:49 +0530

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async options => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'Jonas Schmedtmann <hello@jonas.io>',
+    from: 'Siddhesh Suryawanshi <hello@temp.io>',
     to: options.email,
     subject: options.subject,
     text: options.message
